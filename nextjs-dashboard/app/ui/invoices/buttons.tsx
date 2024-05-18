4720a3e5a300188aa3ec716bc7af50d3a54ab39c
leonah01 <don22010@byui.edu>
2024-05-18 16:12:21 +0300

    dashboard-app
    Signed-off-by: leonah01 <don22010@byui.edu>

--- nextjs-dashboard/app/ui/invoices/buttons.tsx
+++ nextjs-dashboard/app/ui/invoices/buttons.tsx
@@ -1,5 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
   return (
@@ -16,8 +17,8 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
-      className="rounded-md border p-2 hover:bg-gray-100"
+      href={`/dashboard/invoices/${id}/edit`}
+        className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
     </Link>
